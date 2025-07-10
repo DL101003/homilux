238162c91c64dd2195e59c2d882bca62fa3a9557
Hoang Van Loc <hani101003@gmail.com>
2025-07-10 14:44:23 +0700

    Add TanStack-Router React-Query Shadcn TanStack-Table

--- homilux_FE/homilux_fe/src/main.tsx
+++ homilux_FE/homilux_fe/src/main.tsx
@@ -1,10 +1,55 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from '@/routeTree.gen'
+import '@/styles.css'
+import reportWebVitals from '@/reportWebVitals.ts'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient()
+
+export function getContext() {
+  return {
+    queryClient,
+  }
+}
+
+// Create a new router instance
+const router = createRouter({
+  routeTree,
+  context: {
+    ...getContext(),
+  },
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+})
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('app')
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <Provider>
+        <RouterProvider router={router} />
+      </Provider>
+    </StrictMode>,
+  )
+}
+
+reportWebVitals()
